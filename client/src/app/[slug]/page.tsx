9ceae7cf19498416c6f6b0eb2c8e09b619cd2670
FallEn <axlsntz.dev@gmail.com>
2024-03-09 19:20:50 -0600

    fixed errors

--- client/src/app/[slug]/page.tsx
+++ client/src/app/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function SlugRedirect() {
   useEffect(() => {
     const slug = window.location.pathname.split("/")[1];
-    window.open(config.API_URL + slug, "_self");
+    window.open('https://shorttener-production.up.railway.app/' + slug, "_self");
   }, []);
   return;
 }
