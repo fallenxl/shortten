26db0f576ac506939d0ee18368bf6e1631291ace
FallEn <axlsntz.dev@gmail.com>
2024-03-09 19:04:40 -0600

    fixed error urls

--- client/src/app/[slug]/page.tsx
+++ client/src/app/[slug]/page.tsx
@@ -1,10 +1,11 @@
 "use client";
+import config from "@/config";
 import { useEffect } from "react";
 
 export default function SlugRedirect() {
   useEffect(() => {
     const slug = window.location.pathname.split("/")[1];
-    window.open(`http://localhost:4001/${slug}`, "_self");
+    window.open(config.API_URL + slug, "_self");
   }, []);
   return;
 }
