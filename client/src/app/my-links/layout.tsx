114ee235a8d0f269c08475fb7563489a0552610c
FallEn <axlsntz.dev@gmail.com>
2024-03-11 14:20:06 -0600

    added changes

--- client/src/app/my-links/layout.tsx
+++ client/src/app/my-links/layout.tsx
@@ -1,15 +1,18 @@
 "use client";
 import Footer from "@/components/component/footer";
 import Header from "@/components/component/header";
+import { IAppStore } from "@/interfaces";
+import { useSelector } from "react-redux";
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const user = useSelector((state: IAppStore) => state.user);
   return (
     <>
       <div className="bg-gray-50 min-h-screen">
         <div className="max-w-screen-xl mx-auto px-6 md:px-6 pb-16 pt-8">
-          <Header />
+          <Header user={user}/>
           {children}
         </div>
         <Footer />
