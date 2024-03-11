7483e16cc55fc1a5df2fddb829b468bc25b14252
FallEn <axlsntz.dev@gmail.com>
2024-03-11 14:16:27 -0600

    Added changes

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getProfile } from "./services";
-import { extractTokenFromCookie } from "./utils";
 
 
 // This function can be marked `async` if using `await` inside
